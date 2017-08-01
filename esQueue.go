74b646a46a34f79347c1d355d93182f2381f0ade
yireyun <yireyun@126.com>
2017-08-01 16:06:15 +0800

    Removes the direct read memory, Switch to load an atomic operation 移除直接read内存的，改用load原子操作

--- esQueue.go
+++ esQueue.go
@@ -1,6 +1,11 @@
 // esQueue
 package queue
 
+import (
+	"runtime"
+	"sync/atomic"
+)
+
 type esCache struct {
 	value interface{}
 	mark  bool
@@ -25,6 +30,104 @@ func NewQueue(capaciity uint32) *EsQueue {
 
 func (q *EsQueue) Capaciity() uint32 {
 	return q.capaciity
+}
+
+func (q *EsQueue) Quantity() uint32 {
+	var putPos, getPos uint32
+	var quantity uint32
+	getPos = atomic.LoadUint32(&q.getPos)
+	putPos = atomic.LoadUint32(&q.putPos)
+
+	if putPos >= getPos {
+		quantity = putPos - getPos
+	} else {
+		quantity = q.capMod - getPos + putPos
+	}
+
+	return quantity
+}
+
+// put queue functions
+func (q *EsQueue) Put(val interface{}) (ok bool, quantity uint32) {
+	var putPos, putPosNew, getPos, posCnt uint32
+	var cache *esCache
+	capMod := q.capMod
+	for {
+		getPos = atomic.LoadUint32(&q.getPos)
+		putPos = atomic.LoadUint32(&q.putPos)
+
+		if putPos >= getPos {
+			posCnt = putPos - getPos
+		} else {
+			posCnt = capMod - getPos + putPos
+		}
+
+		if posCnt >= capMod {
+			runtime.Gosched()
+			return false, posCnt
+		}
+
+		putPosNew = putPos + 1
+		if atomic.CompareAndSwapUint32(&q.putPos, putPos, putPosNew) {
+			break
+		} else {
+			runtime.Gosched()
+		}
+	}
+
+	cache = &q.cache[putPosNew&capMod]
+
+	for {
+		if !cache.mark {
+			cache.value = val
+			cache.mark = true
+			return true, posCnt + 1
+		} else {
+			runtime.Gosched()
+		}
+	}
+}
+
+// get queue functions
+func (q *EsQueue) Get() (val interface{}, ok bool, quantity uint32) {
+	var putPos, getPos, getPosNew, posCnt uint32
+	var cache *esCache
+	capMod := q.capMod
+	for {
+		putPos = atomic.LoadUint32(&q.putPos)
+		getPos = atomic.LoadUint32(&q.getPos)
+
+		if putPos >= getPos {
+			posCnt = putPos - getPos
+		} else {
+			posCnt = capMod - getPos + putPos
+		}
+
+		if posCnt < 1 {
+			runtime.Gosched()
+			return nil, false, posCnt
+		}
+
+		getPosNew = getPos + 1
+		if atomic.CompareAndSwapUint32(&q.getPos, getPos, getPosNew) {
+			break
+		} else {
+			runtime.Gosched()
+		}
+	}
+
+	cache = &q.cache[getPosNew&capMod]
+
+	for {
+		if cache.mark {
+			val = cache.value
+			cache.value = nil
+			cache.mark = false
+			return val, true, posCnt - 1
+		} else {
+			runtime.Gosched()
+		}
+	}
 }
 
 // round 到最近的2的倍数
